36f8a330df8d56e443fc0f103f5233f381aeb664
Akash Verma <Akash.Verma3@amd.com>
2025-02-21 17:31:17 +0000

    Successful build of locally available CK library in pytorch.

--- cmake/External/ck.cmake
+++ cmake/External/ck.cmake
@@ -1,12 +1,14 @@
+#
 # create INTERFACE target for CK shared library
+#
 
 # create INTERFACE target
 add_library(__ck_lib INTERFACE)
 
 # specify path to shared library
-target_link_libraries(__ck_lib INTERFACE /tmp/pytorch/ck_lib_tmp/ck_shared_lib.so)
+target_link_libraries(__ck_lib INTERFACE ${CMAKE_SOURCE_DIR}/ck_lib_tmp/ck_shared_lib.so)
 
 # specify include directories
-target_include_directories(__ck_lib INTERFACE /tmp/pytorch/ck_lib_tmp/includes/01_fmha
-	/tmp/pytorch/ck_lib_tmp/includes/include 
-	/opt/rocm/include)
+#target_include_directories(__ck_lib INTERFACE ${CMAKE_SOURCE_DIR}/ck_lib_tmp/includes/01_fmha
+#${CMAKE_SOURCE_DIR}/ck_lib_tmp/includes/include 
+#/opt/rocm/include)
